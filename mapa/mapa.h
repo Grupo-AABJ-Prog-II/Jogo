276cb67cee68675a9d632c3c51c3c3bcea7e0b97
Alonestrofs <61604186+Alonestrofs@users.noreply.github.com>
2025-12-10 23:01:19 -0300

    Add files via upload

---
+++ mapa/mapa.h
@@ -0,0 +1,69 @@
+#ifndef MAPA_H
+#define MAPA_H
+
+#include "raylib.h"
+
+// Definições
+#define LINHAS_MAPA 20
+#define COLUNAS_MAPA 40
+#define ALTURA_HUD 60 
+
+// Estruturas
+typedef struct {
+    int x;
+    int y;
+} Posicao;
+
+typedef struct {
+    char **grade;// Matriz dinâmica
+    int linhas;
+    int colunas;
+    Posicao inicioPacman;
+    Posicao spawnFantasma;
+    int temSpawn;
+    Posicao *portais; // Vetor dinâmico
+    int *conexoes; // Vetor dinâmico de IDs
+    int qtdPortais;
+    
+    // Estatísticas
+    int qtdPointPellets;
+    int totalPellets;// Total de itens para comer (. + o + +)
+} Mapa;
+
+typedef struct {
+    Texture2D parede;
+    Texture2D pastilha;
+    Texture2D superPastilha;
+    Texture2D pacman;
+    Texture2D fantasma;
+    Texture2D portal;
+} Sprites;
+
+//mapa no geral
+Mapa* CarregarMapa(const char *nomeArquivo);
+
+// Limpa memória (importante!)
+void LiberarMapa(Mapa *mapa);
+
+// Carrega as imagens
+void CarregarSprites(Sprites *sprites);
+void DescarregarSprites(Sprites *sprites);
+
+// Função de desenho que se adapta ao tamanho do bloco
+void DesenharMapa(Mapa *mapa, Sprites *sprites, int tamanhoBloco);
+
+// Auxiliares úteis para a movimentação
+double CalcularDistancia(Posicao p1, Posicao p2);
+
+// Funções de Save/Load e Utilitários (adicionar mais depois?)
+
+// Salva o jogo completo
+int SalvarEstadoMapa(Mapa *mapa, const char *nomeArquivo, Posicao pacmanAtual, Posicao *fantasmas, int qtdFantasmas);
+
+// Carrega o jogo salvo e retorna posições dinâmicas via ponteiros
+Mapa* CarregarEstadoMapa(const char *nomeArquivo, Posicao *outPacman, Posicao **outFantasmas, int *outQtdFantasmas);
+
+// Verifica condição de vitória
+int ContarPastilhasRestantes(Mapa *mapa);
+
+#endif
